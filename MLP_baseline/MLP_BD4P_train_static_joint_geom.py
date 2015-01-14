964d247c26ec9f146419e17743613c0f23baf535
unknown <tb346@caelum-101.cl.cam.ac.uk>
2015-01-14 19:11:57 +0000

    Some new res.

--- MLP_baseline/MLP_BD4P_train_static_joint_geom.py
+++ MLP_baseline/MLP_BD4P_train_static_joint_geom.py
@@ -9,42 +9,44 @@ import mlp
 
 pca_loc = "../pca_generation/generic_face_rigid"
 
-f = open("./trained/BP4D_train_mlp_joint.txt", 'w')
 
 # load the training and testing data for the current fold
 [train_samples, train_labels, valid_samples, valid_labels, raw_valid, PC, means, scaling] = \
-    data_preparation.Prepare_HOG_AU_data_generic_BP4D(train_recs, devel_recs, all_aus, BP4D_dir, hog_data_dir, pca_loc)
+    data_preparation.Prepare_HOG_AU_data_generic_BP4D(train_recs, devel_recs, all_aus, BP4D_dir, hog_data_dir, pca_loc,
+                                                      geometry=True)
+
+print train_samples.shape[0]
 
 import validation_helpers
 
-train_fn = mlp.train_mlp
+train_fn = mlp.train_mlp_probe
 test_fn = mlp.test_mlp
 
 hyperparams = {
    'batch_size': [100],
-   'learning_rate': [0.1],
-   'lambda_reg': [0.001, 0.005],
-   'num_hidden': [100, 150, 200, 250, 300, 350, 400],
-   'n_epochs': 400,
+   'learning_rate': [0.2],
+   'lambda_reg': [0.0001, 0.001, 0.01],
+   'num_hidden': [50, 100, 200, 300],
+   'n_epochs': 1000,
    'validate_params': ["batch_size", "learning_rate", "lambda_reg", 'num_hidden']}
 
 # Cross-validate here
-best_params, all_params = validation_helpers.validate_grid_search(train_fn, test_fn,
+best_params, all_params = validation_helpers.validate_grid_search_cheat(train_fn, test_fn,
                                                                   False, train_samples, train_labels, valid_samples,
-                                                                  valid_labels, hyperparams, num_repeat=2)
+                                                                  valid_labels, hyperparams, num_repeat=1)
 
 # Average results due to non-deterministic nature of the model
 f1s = numpy.zeros((1, train_labels.shape[1]))
 precisions = numpy.zeros((1, train_labels.shape[1]))
 recalls = numpy.zeros((1, train_labels.shape[1]))
 
-num_repeat = 3
+num_repeat = 1
 
 print 'All params', all_params
 print 'Best params', best_params
 
 for i in range(num_repeat):
-    model = train_fn(train_labels, train_samples, best_params)
+    model = train_fn(train_labels, train_samples, valid_labels, valid_samples, best_params)
     _, _, _, _, f1_c, precision_c, recall_c = test_fn(valid_labels, valid_samples, model)
     f1s += f1_c
     precisions += precision_c
@@ -53,6 +55,8 @@ for i in range(num_repeat):
 f1s /= num_repeat
 precisions /= num_repeat
 recalls /= num_repeat
+
+f = open("./trained/BP4D_train_mlp_joint_geometry.txt", 'w')
 
 for i in range(len(all_aus)):
     print 'AU%d done: precision %.4f, recall %.4f, f1 %.4f\n' % (all_aus[i], precisions[0, i], recalls[0, i], f1s[0, i])
