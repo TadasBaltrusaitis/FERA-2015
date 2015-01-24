640224ae6863b9e00ea857c45980846cb4d7017d
Tadas Baltrusaitis <tadyla@gmail.com>
2015-01-24 09:19:12 +0000

    Preparing final SEMAINE submission.

--- runners/evaluate_SEMAINE.m
+++ runners/evaluate_SEMAINE.m
@@ -4,7 +4,7 @@ find_SEMAINE;
 
 aus_SEMAINE = [2 12 17 25 28 45];
 
-root = 'out_SEMAINE_dynamic/';
+root = 'out_SEMAINE_best/';
 
 [ labels_gt, valid_ids, vid_ids] = extract_SEMAINE_labels(SEMAINE_dir, devel_recs, aus_SEMAINE);
 labels_gt = cat(1, labels_gt{:});
