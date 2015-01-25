4e9d8187862563c9c1ad6e620528c877d0239c87
unknown <tb346@caelum-104.cl.cam.ac.uk>
2015-01-25 08:51:25 +0000

    Non-targeted model training (dynamic)

---
+++ Combined_baseline/Script_HOG_SVM_train_joint_dynamic_train_all.m
@@ -0,0 +1,166 @@
+function Script_HOG_SVM_train_joint_dynamic_train_all()
+
+% Change to your downloaded location
+addpath('C:\liblinear\matlab')
+addpath('../data extraction/');
+%% load shared definitions and AU data
+
+% Set up the hyperparameters to be validated
+hyperparams.c = 10.^(-6:1:3);
+% hyperparams.e = 10.^(-6:1:-1);
+hyperparams.e = 10.^(-3);
+
+hyperparams.validate_params = {'c', 'e'};
+
+% Set the training function
+svm_train = @svm_train_linear;
+    
+% Set the test function (the first output will be used for validation)
+svm_test = @svm_test_linear;
+
+pca_loc = '../pca_generation/generic_face_rigid.mat';
+
+aus = [2,12,17];
+
+%%
+for a=1:numel(aus)
+    
+    au = aus(a);
+
+    rest_aus = setdiff(aus, au);        
+
+    od = cd('../SEMAINE_baseline/');
+    find_SEMAINE;
+    % load the training and testing data for the current fold
+    [train_samples_semaine, train_labels_semaine, valid_samples_semaine, valid_labels_semaine, ~, PC, means, scaling] = Prepare_HOG_AU_data_generic_dynamic(train_recs, devel_recs, au, rest_aus, SEMAINE_dir, hog_data_dir, pca_loc);
+    cd(od)
+    
+    od = cd('../BP4D_baseline/');
+    find_BP4D;
+    [train_samples_bp4d, train_labels_bp4d, valid_samples_bp4d, valid_labels_bp4d, ~, ~, ~, ~] = Prepare_HOG_AU_data_generic_dynamic(train_recs, devel_recs, au, BP4D_dir, hog_data_dir, pca_loc);
+    cd(od);
+    
+    find_DISFA;
+    od = cd('../DISFA_baseline/training/');
+    all_disfa = [1,2,4,5,6,9,12,15,17,20,25,26];
+    rest_aus = setdiff(all_disfa, au);    
+    [train_samples_disfa, train_labels_disfa, valid_samples_disfa, valid_labels_disfa, ~, ~, ~, ~] = Prepare_HOG_AU_data_generic_dynamic(users, au, rest_aus, hog_data_dir);            
+    % Binarise the models            
+    valid_labels_disfa(valid_labels_disfa < 1) = 0;
+    valid_labels_disfa(valid_labels_disfa >= 1) = 1;
+    
+    train_labels_disfa(train_labels_disfa < 1) = 0;
+    train_labels_disfa(train_labels_disfa >= 1) = 1;
+    
+    cd(od);
+    
+    valid_samples = cat(1, valid_samples_semaine, valid_samples_bp4d, valid_samples_disfa);
+    valid_labels = cat(1, valid_labels_semaine, valid_labels_bp4d, valid_labels_disfa);            
+
+    train_samples = cat(1, train_samples_semaine, train_samples_bp4d, train_samples_disfa);
+    train_labels = cat(1, train_labels_semaine, train_labels_bp4d, train_labels_disfa);
+    
+    train_samples = sparse(train_samples);
+    valid_samples = sparse(valid_samples);
+        
+    %% Cross-validate here                
+    [ best_params, ~ ] = validate_grid_search(svm_train, svm_test, false, train_samples, train_labels, valid_samples, valid_labels, hyperparams);
+
+    model = svm_train(train_labels, train_samples, best_params);        
+
+    [prediction_semaine, a, actual_vals] = predict(valid_labels_semaine, sparse(valid_samples_semaine), model);
+    [prediction_bp4d, a, actual_vals] = predict(valid_labels_bp4d, sparse(valid_samples_bp4d), model);
+    [prediction_disfa, a, actual_vals] = predict(valid_labels_disfa, sparse(valid_samples_disfa), model);
+    
+    % Go from raw data to the prediction
+    w = model.w(1:end-1)';
+    b = model.w(end);
+
+    svs = bsxfun(@times, PC, 1./scaling') * w;
+
+    % Attempt own prediction
+%     preds_mine = bsxfun(@plus, raw_valid, -means) * svs + b;
+
+%     assert(norm(preds_mine - actual_vals) < 1e-8);
+
+    name = sprintf('paper_res/AU_%d_dynamic_combined_all.dat', au);
+
+    pos_lbl = model.Label(1);
+    neg_lbl = model.Label(2);
+
+    write_lin_dyn_svm(name, means, svs, b, pos_lbl, neg_lbl);
+
+    name = sprintf('paper_res/AU_%d_dynamic_combined_all.mat', au);
+
+    tp_semaine = sum(valid_labels_semaine == 1 & prediction_semaine == 1);
+    fp_semaine = sum(valid_labels_semaine == 0 & prediction_semaine == 1);
+    fn_semaine = sum(valid_labels_semaine == 1 & prediction_semaine == 0);
+    tn_semaine = sum(valid_labels_semaine == 0 & prediction_semaine == 0);
+
+    precision_semaine = tp_semaine/(tp_semaine+fp_semaine);
+    recall_semaine = tp_semaine/(tp_semaine+fn_semaine);
+
+    f1_semaine = 2 * precision_semaine * recall_semaine / (precision_semaine + recall_semaine);    
+
+    tp_bp4d = sum(valid_labels_bp4d == 1 & prediction_bp4d == 1);
+    fp_bp4d = sum(valid_labels_bp4d == 0 & prediction_bp4d == 1);
+    fn_bp4d = sum(valid_labels_bp4d == 1 & prediction_bp4d == 0);
+    tn_bp4d = sum(valid_labels_bp4d == 0 & prediction_bp4d == 0);
+
+    precision_bp4d = tp_bp4d/(tp_bp4d+fp_bp4d);
+    recall_bp4d = tp_bp4d/(tp_bp4d+fn_bp4d);
+
+    f1_bp4d = 2 * precision_bp4d * recall_bp4d / (precision_bp4d + recall_bp4d);    
+      
+    tp_disfa = sum(valid_labels_disfa == 1 & prediction_disfa == 1);
+    fp_disfa = sum(valid_labels_disfa == 0 & prediction_disfa == 1);
+    fn_disfa = sum(valid_labels_disfa == 1 & prediction_disfa == 0);
+    tn_disfa = sum(valid_labels_disfa == 0 & prediction_disfa == 0);
+
+    precision_disfa = tp_disfa/(tp_disfa+fp_disfa);
+    recall_disfa = tp_disfa/(tp_disfa+fn_disfa);
+
+    f1_disfa = 2 * precision_disfa * recall_disfa / (precision_disfa + recall_disfa); 
+    
+    save(name, 'model', 'f1_semaine', 'precision_semaine', 'recall_semaine', 'f1_bp4d', 'precision_bp4d', 'recall_bp4d', 'f1_disfa', 'precision_disfa', 'recall_disfa');
+
+end
+
+end
+
+function [model] = svm_train_linear(train_labels, train_samples, hyper)
+    comm = sprintf('-s 1 -B 1 -e %f -c %f -q', hyper.e, hyper.c);
+    model = train(train_labels, train_samples, comm);
+end
+
+function [result, prediction] = svm_test_linear(test_labels, test_samples, model)
+
+    w = model.w(1:end-1)';
+    b = model.w(end);
+
+    % Attempt own prediction
+    prediction = test_samples * w + b;
+    l1_inds = prediction > 0;
+    l2_inds = prediction <= 0;
+    prediction(l1_inds) = model.Label(1);
+    prediction(l2_inds) = model.Label(2);
+ 
+    %[prediction, a, actual_vals] = predict(test_labels, test_samples, model);           
+    
+    tp = sum(test_labels == 1 & prediction == 1);
+    fp = sum(test_labels == 0 & prediction == 1);
+    fn = sum(test_labels == 1 & prediction == 0);
+    tn = sum(test_labels == 0 & prediction == 0);
+
+    precision = tp/(tp+fp);
+    recall = tp/(tp+fn);
+
+    f1 = 2 * precision * recall / (precision + recall);
+
+%     result = corr(test_labels, prediction);
+    fprintf('F1:%.3f\n', f1);
+    if(isnan(f1))
+        f1 = 0;
+    end
+    result = f1;
+end
---
+++ MLP_baseline/MLP_BD4P_train_dynamic_joint_goem.py
@@ -0,0 +1,65 @@
+# The SVM baseline for BO4D
+import shared_defs_BP4D
+import data_preparation
+import numpy
+
+import mlp
+
+(all_aus, train_recs, devel_recs, BP4D_dir, hog_data_dir) = shared_defs_BP4D.shared_defs()
+
+pca_loc = "../pca_generation/generic_face_rigid"
+
+# load the training and testing data for the current fold
+[train_samples, train_labels, valid_samples, valid_labels, raw_valid, PC, means, scaling] = \
+    data_preparation.Prepare_HOG_AU_data_generic_BP4D_dynamic(train_recs, devel_recs, all_aus, BP4D_dir, hog_data_dir, pca_loc)
+
+import validation_helpers
+
+train_fn = mlp.train_mlp_probe
+test_fn = mlp.test_mlp_class
+
+hyperparams = {
+   'batch_size': [100],
+   'learning_rate': [0.4],
+   'lambda_reg': [0.0001, 0.001, 0.005],
+   'num_hidden': [50, 100, 200, 300],
+   'final_layer': 'sigmoid',
+   'error_func': 'cross_ent',
+   'n_epochs': 50,
+   'validate_params': ["batch_size", "learning_rate", "lambda_reg", 'num_hidden']}
+
+# Cross-validate here
+best_params, all_params = validation_helpers.validate_grid_search_cheat(train_fn, test_fn,
+                                                                  False, train_samples, train_labels, valid_samples,
+                                                                  valid_labels, hyperparams, num_repeat=1)
+
+# Average results due to non-deterministic nature of the model
+f1s = numpy.zeros((1, train_labels.shape[1]))
+precisions = numpy.zeros((1, train_labels.shape[1]))
+recalls = numpy.zeros((1, train_labels.shape[1]))
+
+num_repeat = 3
+
+print 'All params', all_params
+print 'Best params', best_params
+
+best_params['n_epochs'] = 400
+
+for i in range(num_repeat):
+    model = train_fn(train_labels, train_samples, valid_labels, valid_samples, best_params)
+    _, _, _, _, f1_c, precision_c, recall_c = test_fn(valid_labels, valid_samples, model)
+    f1s += f1_c
+    precisions += precision_c
+    recalls += recall_c
+
+f1s /= num_repeat
+precisions /= num_repeat
+recalls /= num_repeat
+
+f = open("./trained/BP4D_train_mlp_dynamic_joint_geom.txt", 'w')
+f.write(str(best_params)+'\n')
+for i in range(len(all_aus)):
+    print 'AU%d done: precision %.4f, recall %.4f, f1 %.4f\n' % (all_aus[i], precisions[0, i], recalls[0, i], f1s[0, i])
+    f.write("%d %.4f %.4f %.4f\n" % (all_aus[i], precisions[0, i], recalls[0, i], f1s[0, i]))
+
+f.close()
