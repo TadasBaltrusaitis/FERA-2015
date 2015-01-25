bfe9df6f2cfcb6c7356742f6e0d8f907a03ccab5
Tadas Baltrusaitis <tadyla@gmail.com>
2015-01-25 17:12:15 +0000

    Correction to the batch script.

--- C++ models/script_au_pred.bat
+++ C++ models/script_au_pred.bat
@@ -1,6 +1,6 @@
 @echo off
 
 IF "SEMAINE"=="%2" set AU_loc="./AU_predictors/AU_SVM_SEMAINE_best.txt"
-IF "BP4D"=="%2" set AU_loc="./AU_predictors/AU_SVM_BP4D_stat_geom.txt"
+IF "BP4D"=="%2" set AU_loc="./AU_predictors/AU_SVM_BP4D_best.txt"
 
 "%~dp0./Release/AUPrediction.exe" -q -auloc %AU_loc% -fx 2000 -fy 2000 -ftxt %1 -rigid -asvid -simscale 0.7 -simsize 112 -oausclass %4 -oausreg %5 -oausregseg %6
