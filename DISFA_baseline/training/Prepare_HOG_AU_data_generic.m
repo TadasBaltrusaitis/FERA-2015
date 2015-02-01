da2ceade4dad2d954d0ab492603f9a248c92866e
Tadas Baltrusaitis <tadyla@gmail.com>
2015-02-01 13:52:10 +0000

    working on cleverer DISFA sampling.

--- DISFA_baseline/training/Prepare_HOG_AU_data_generic.m
+++ DISFA_baseline/training/Prepare_HOG_AU_data_generic.m
@@ -26,6 +26,17 @@ for i=1:numel(train_users)
     input_train_label_files{i} = [root, '/ActionUnit_Labels/', train_users{i}, '/', train_users{i}];
 end
 
+% Extracting the labels
+labels_train = extract_au_labels(input_train_label_files, au_train);
+    
+labels_other = zeros(size(labels_train,1), numel(rest_aus));
+
+% This is used to pick up activity of other AUs for a more 'interesting'
+% data split and not only neutral expressions for negative samples    
+for i=1:numel(rest_aus)
+    labels_other(:,i) = extract_au_labels(input_train_label_files, rest_aus(i));
+end
+    
 % Reading in the HOG data
 [train_geom_data] = Read_geom_files(train_users, hog_data_dir);
 
@@ -36,9 +47,6 @@ train_appearance_data = cat(2, train_appearance_data, train_geom_data);
 % Getting the indices describing the splits (full version)
 [training_inds, valid_inds, split] = construct_indices(vid_ids_train, train_users);
 
-% Extracting the labels
-labels_train = extract_au_labels(input_train_label_files, au_train);
-
 % can now extract the needed training labels (do not rebalance validation
 % data)
 labels_valid = labels_train(valid_inds);
@@ -47,16 +55,37 @@ labels_valid = labels_train(valid_inds);
 pos_count = sum(labels_train(training_inds) > 0);
 neg_count = sum(labels_train(training_inds) == 0);
 
-inds_train = 1:size(labels_train,1);
-neg_samples = inds_train(labels_train == 0 & training_inds);
-to_rem = round(neg_count -  pos_count);
-neg_samples_to_rem = neg_samples(round(linspace(1, size(neg_samples,2), to_rem)));
+reduced_inds = false(size(labels_train,1),1);
+reduced_inds(labels_train > 0 & training_inds) = true;
 
-% Get rid of non tracked frames
-training_inds = true(size(labels_train,1),1);
-training_inds(valid_inds) = false;
-training_inds(~tracked_inds_hog) = false;
-training_inds(neg_samples_to_rem) = false;
+% make sure the same number of positive and negative samples is taken
+pos_count = sum(labels_train(training_inds) > 0);
+neg_count = sum(labels_train(training_inds) == 0);
+
+num_other = floor(pos_count / (size(labels_other, 2)));
+
+inds_all = 1:size(labels_train,1);
+
+for i=1:size(labels_other, 2)+1
+   
+    if(i > size(labels_other, 2))
+        % fill the rest with a proportion of neutral
+        inds_other = inds_all(sum(labels_other(training_inds,:),2)==0 & ~labels_train & training_inds);   
+        num_other_i = min(numel(inds_other), pos_count - sum(labels_train(reduced_inds,:)==0));     
+    else
+        % take a proportion of each other AU
+        inds_other = inds_all(labels_other(:, i) & ~labels_train & training_inds );      
+        num_other_i = min(numel(inds_other), num_other);        
+    end
+    inds_other_to_keep = inds_other(round(linspace(1, numel(inds_other), num_other_i)));
+    reduced_inds(inds_other_to_keep) = true;
+    
+end
+
+% Remove invalid ids based on CLM failing or AU not being labelled
+reduced_inds(~tracked_inds_hog) = false;
+
+training_inds(~reduced_inds) = false;
 
 labels_train = labels_train(training_inds);
 
